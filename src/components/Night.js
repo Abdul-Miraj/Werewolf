770d0a412ac532fb171c84107efb4f0a647a18bd
Osama <osama_alhaq@hotmail.com>
2018-01-04 11:55:10 -0500

    Make player awake into separate component

--- src/components/Night.js
+++ src/components/Night.js
@@ -5,7 +5,7 @@ import * as actions from '../actions';
 import CountdownCircle from 'react-native-countdown-circle';
 import PlayerSelection from './PlayerSelection';
 import shuffle from 'shuffle-array';
-import roles from '../reducers/RoleList.json';
+import WakePlayer from './WakePlayer';
 
 // Home screen that imports all components
 class Night extends Component {
@@ -18,12 +18,6 @@ class Night extends Component {
             username: 'OSAMAALHAQ'
         };
     }
-
-    // Get my player object based on my username
-
-    // Get the role object
-
-    // check if the role server gave matches my role
 
     componentDidMount() {
         console.log(this.props);
@@ -44,7 +38,7 @@ class Night extends Component {
         return (
             <View style={styles.night}>
                 <Text style={styles.Title}>Night {this.state.night}</Text>
-                <CountdownCircle
+                <CountdownCircle // for testing only
                     seconds={8}
                     radius={0}
                     onTimeElapsed={() => this.setState({
@@ -57,40 +51,10 @@ class Night extends Component {
     };
 
     renderWake = () => {
-        const { header, highlight, timer, night, selection } = styles;
-        let roleIndex = roles.findIndex(x => x.role == this.state.serverRole);
-        const role = roles[roleIndex];
         return (
-            <View style={{ flex: 1 }} >
-                <View style={{ paddingTop: 40, flexDirection: 'row' }}>
-                    <Text style={header}>
-                        <Text style={highlight} >{role.role}</Text>:  {role.description}
-                    </Text>
-                    <View style={timer}>
-                        <CountdownCircle
-                            seconds={10}
-                            radius={25}
-                            borderWidth={4}
-                            color="#4fd09a"
-                            bgColor="#222c31"
-                            textStyle={{ fontSize: 20, color: '#4fd09a' }}
-                            onTimeElapsed={() => this.setState({
-                                isWoke: !this.state.isWoke,
-                            })} // update the state of the players woken up
-                        />
-                    </View>
-                </View>
-                <View style={night}>
-                    <ScrollView contentContainerStyle={{ flexGrow: 1 }} >
-                        <PlayerSelection players={this.props.players} role={this.state.serverRole} />
-                    </ScrollView>
-                    <View style={selection}>
-                        <Text style={highlight} >
-                            You have selected {this.props.night[this.state.serverRole] == null ? '' : this.props.night[this.state.serverRole]}
-                        </Text>
-                    </View>
-                </View>
-            </View>
+            <WakePlayer night={this.props.night[this.state.serverRole]} role={this.state.serverRole}>
+                <PlayerSelection players={shuffle(this.props.players)} role={this.state.serverRole} />
+            </WakePlayer>
         );
     };
 
@@ -126,31 +90,6 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
     },
-    header: {
-        width: '80%',
-        padding: 20,
-        paddingTop: 10,
-        fontWeight: '500',
-        color: '#fff',
-        fontSize: 18,
-        textAlign: 'left'
-    },
-    timer: {
-        flex: 1,
-        alignSelf: 'flex-end',
-        alignItems: 'flex-end',
-        padding: 20,
-        paddingTop: 0
-    },
-    highlight: {
-        fontSize: 22,
-        color: '#4fd09a'
-    },
-    selection: {
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
-        padding: 8
-    }
 };
 
 // Hide the navigation
---
+++ src/components/WakePlayer.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    ScrollView
+} from 'react-native';
+import CountdownCircle from 'react-native-countdown-circle';
+import roles from '../reducers/RoleList.json';
+
+// Component thats decide which players can be voted for
+const WakePlayer = (props) => {
+    const { header, highlight, timer, night, selection } = styles;
+    let roleIndex = roles.findIndex(x => x.role == props.role);
+    const role = roles[roleIndex];
+    return (
+        <View style={{ flex: 1 }} >
+            <View style={{ paddingTop: 40, flexDirection: 'row' }}>
+                <Text style={header}>
+                    <Text style={highlight} >{role.role}</Text>:  {role.description}
+                </Text>
+                <View style={timer}>
+                    <CountdownCircle
+                        seconds={10}
+                        radius={25}
+                        borderWidth={4}
+                        color="#4fd09a"
+                        bgColor="#222c31"
+                        textStyle={{ fontSize: 20, color: '#4fd09a' }}
+                        onTimeElapsed={() => console.log('Timer ended')} // update the state of the players woken up
+                    />
+                </View>
+            </View>
+            <View style={night}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }} >
+                    {props.children}
+                </ScrollView>
+                <View style={selection}>
+                    <Text style={highlight} >
+                        You have selected {props.night == null ? '' : props.night}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const styles = {
+    night: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        width: '80%',
+        padding: 20,
+        paddingTop: 10,
+        fontWeight: '500',
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'left'
+    },
+    timer: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'flex-end',
+        padding: 20,
+        paddingTop: 0
+    },
+    highlight: {
+        fontSize: 22,
+        color: '#4fd09a'
+    },
+    selection: {
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end',
+        padding: 8
+    }
+};
+
+export default WakePlayer;
