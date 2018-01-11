858461d79650a0f5ab091b4f44cf7b0f51bbe8e5
Osama <osama_alhaq@hotmail.com>
2018-01-11 00:10:31 -0500

    Able to render day once everyone is woken up

--- src/components/Night.js
+++ src/components/Night.js
@@ -20,7 +20,7 @@ class Night extends Component {
         };
     }
 
-    // create the new info object and pass to server each time 
+    // create the new night object and pass to server each time 
     componentWillMount() {
         shuffle(this.props.players);
         const night = {
@@ -34,6 +34,31 @@ class Night extends Component {
         this.props.addNight(night);
     }
 
+    // when user votes update their screen to display night 
+    componentWillReceiveProps(nextProps) {
+        if (this.props.night != nextProps.night) {
+            console.log("PROPS: ", nextProps.night);
+            let wakeUp = '';
+            let nightObj = nextProps.night[nextProps.night.length - 1];
+            if (nightObj != undefined) {
+                // iterate though each role to see which hasnt woken up
+                for (let i = 0; i < roles.length; i++) {
+                    if (nightObj[roles[i].role] == "") {
+                        this.setState({ awakeRole: roles[i].role });
+                        wakeUp = roles[i].role;
+                        break;
+                    }
+                }
+            }
+            // go to day once everyone has been called
+            if (wakeUp == '') {
+                this.props.navigation.dispatch({ type: 'Day' });
+            }
+
+        }
+    }
+
+    // functino to display night screen
     renderNight = () => {
         return (
             <View style={styles.night}>
@@ -42,6 +67,7 @@ class Night extends Component {
         );
     };
 
+    // functino that passes called role and lets players do selection
     renderWake = () => {
         return (
             <WakePlayer night={this.props.night[this.state.awakeRole]} role={this.state.awakeRole}>
@@ -50,27 +76,13 @@ class Night extends Component {
         );
     };
 
+    // function to check if my role has been called
     wakePlayer = () => {
         // if myrole is same as server then display selection
         let players = this.props.players;
         let playerIndex = players.findIndex(x => x.id == this.state.id);
-        let wakeUp = '';
-        let nightObj = this.props.night[this.props.night.length -1];
-        console.log("Night: ", nightObj);
-        if (nightObj != undefined) {
-            // iterate though each role to see which hasnt woken up
-            for (let i = 0; i < roles.length; i++) {
-                if (nightObj[roles[i].role] == "") {
-                    wakeUp = roles[i].role;
-                    break;
-                }
-            }
-        }
-        if (wakeUp == '') {
-            // render day
-        }
         // if role to wake up matches my role return true
-        return players[playerIndex].role == wakeUp;
+        return players[playerIndex].role == this.state.awakeRole;
     };
 
     render() {
